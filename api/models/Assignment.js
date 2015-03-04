43298ee4bc9b6d74c8fc7411a9ed08de9c2c52d0
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-03-03 18:54:17 -0600

    updates

--- api/models/Assignment.js
+++ api/models/Assignment.js
@@ -8,8 +8,9 @@
 module.exports = {
 
   attributes: {
-      name: { type: 'string', unique: true },
-      url: { type: 'string', unique: false },
-      dueAt: { type: 'datetime', unique: false },
+  	name: { type: 'string', unique: true },
+  	dueAt: { type: 'datetime', unique: false },
+  	url: { type: 'string', unique: false }
   }
 };
+
